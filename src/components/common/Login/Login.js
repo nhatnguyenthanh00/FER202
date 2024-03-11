881195af844581d82d85f54a87e23ae0ba6a94aa
NHAT <thanhdeapool@gmail.com>
2024-03-11 14:44:22 +0700

    Login done

--- src/components/common/Login/Login.js
+++ src/components/common/Login/Login.js
@@ -3,88 +3,89 @@ import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import "./login.css";
 import { useNavigate } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 const Login = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [users, setUsers] = useState([]);
-  const navigate = useNavigate();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [users, setUsers] = useState([]);
+    const navigate = useNavigate();
+    const [showToast, setShowToast] = useState(false);
 
-  useEffect(() => {
-    fetch("http://localhost:9999/user")
-      .then((response) => response.json())
-      .then((data) => setUsers(data));
-  }, []);
+    useEffect(() => {
+        fetch("http://localhost:9999/user")
+            .then((response) => response.json())
+            .then((data) => setUsers(data));
+    }, []);
 
-  const handleUsernameChange = (e) => {
-    setUsername(e.target.value);
-  };
+    const handleUsernameChange = (e) => {
+        setUsername(e.target.value);
+    };
 
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    let indexUser = users.findIndex(
-      (user) => user.username == username && user.password == password
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let indexUser = users.findIndex(
+            (user) => user.username == username && user.password == password
+        );
+        if (indexUser === -1 || users[indexUser].status == 0) {
+            toast.error("User or password not found");
+        } else {
+            sessionStorage.setItem("user", JSON.stringify(users[indexUser]));
+            // toast.success("Login successful");
+            console.log(sessionStorage.getItem("user"));
+            toast.success("Login successful", {
+                autoClose: 500, // Thời gian tự đóng toast (ms)
+                onClose: () => navigate("/")
+              });
+            
+        }
+
+        // Reset the form
+        setUsername("");
+        setPassword("");
+    };
+
+
+    return (
+        <div className="login-container">
+            <ToastContainer />
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="username">Username:</label>
+                    <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        value={username}
+                        onChange={handleUsernameChange}
+                        required
+                    />
+                </div>
+                <div className="form-group mb-0">
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        value={password}
+                        onChange={handlePasswordChange}
+                        required
+                    />
+                </div>
+                <div className="d-flex justify-content-start pb-3 mr-2" style={{ marginTop: "10px" }}>
+                    <a href="/signup" className="btn btn-primary">Sign up</a>
+                </div>
+                <div className="form-group">
+                    <button type="submit" className="login-button">
+                        Login
+                    </button>
+                </div>
+            </form>
+        </div>
     );
-    if (indexUser === -1 || users[indexUser].status == 0) {
-      toast.error("User or password not found");
-    } else {
-      sessionStorage.setItem("user", JSON.stringify(users[indexUser]));
-      toast.success("Login successful");
-      navigate("/")
-    //   if (users[indexUser].roll == 1) {
-    //     navigate("/");
-    //   }
-    //   if (users[indexUser].roll == 1) {
-    //     navigate("/dashboard");
-    //   }
-    }
-
-    // Reset the form
-    setUsername("");
-    setPassword("");
-  };
-
-  return (
-    <div className="login-container">
-      <ToastContainer />
-      <img src="Images/logo.png" className="card-img-top"></img>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={username}
-            onChange={handleUsernameChange}
-            required
-          />
-        </div>
-        <div className="form-group mb-0">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={handlePasswordChange}
-            required
-          />
-        </div>
-        <div className="d-flex float-right pb-3 mr-2">
-          <a href="\signup">Sign up </a>
-        </div>
-        <div className="form-group">
-          <button type="submit" className="login-button">
-            Login
-          </button>
-        </div>
-      </form>
-    </div>
-  );
 };
 
 export default Login;
